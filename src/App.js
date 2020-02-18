0a43821efae053823a25f168400a2ebbb9f55a43
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2020-02-18 09:06:13 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Rank from './components/Rank/Rank'
+import Logo from './components/Logo/Logo'
+import 'tachyons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: 'f3e013455e1742eab3618934c65ac9bc'
+ });
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      imageURL: ''
+      }
+    }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      //this.state.imageURL wouldn't worked here bc setState is Async
+      function(response) {
+        // do something with response
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)//outputs[0].data.regions[0].
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions}/>
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
+        <FaceRecognition imageURL= {this.state.imageURL}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({imageURL}) => {
+  return (
+    <div className='center ma'>
+      <div className= "absolute mt2">
+        <img id='inputimage' alt='' src={imageURL} width='500px' heigh='auto'/>
+      </div>
+    </div>
+  );
+}
+
+export default FaceRecognition;
