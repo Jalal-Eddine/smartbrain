5f561acdaa2cdfd1b8837262e1ea7c803989c6ef
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2020-02-18 10:50:59 +0100

    update

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import Rank from './components/Rank/Rank'
 import Logo from './components/Logo/Logo'
 import 'tachyons'
 
+//Part of Clarifai API way of using
 const app = new Clarifai.App({
   apiKey: 'f3e013455e1742eab3618934c65ac9bc'
  });
 
+// particules API for the interactive background 
 const particlesOptions = {
   particles: {
     number: {
@@ -30,25 +32,46 @@ class App extends Component {
     super()
     this.state = {
       input: '',
-      imageURL: ''
+      imageURL: '',
+      box: {}
       }
     }
-
+//Listen to Input change
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
-
+// calculate the location of the face based on the API data recieved
+//it recieve an object of the percentage of the face location 
+  calculateFaceLocation = (data) => {
+    const clariFaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clariFaiFace.left_col * width,
+      topRow: clariFaiFace.top_row * height,
+      rightCol: width - (clariFaiFace.right_col * width),
+      bottomRow: height - (clariFaiFace.bottom_row * height)
+    }
+  }
+// update the state of the box 
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
+//listen to click change
+//connect to Clarifai API
+//setState is Async function
+//this.state.imageURL wouldn't worked in .predict() bc setState is Async
+// response is an array of percentage
+// calculateFaceLocation turn it to usable values which passed to the state by displayFaceBox
   onButtonSubmit = () => {
     this.setState({imageURL: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      //this.state.imageURL wouldn't worked here bc setState is Async
-      function(response) {
-        // do something with response
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)//outputs[0].data.regions[0].
-      },
-      function(err) {
-        // there was an error
-      }
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => {
+      this.displayFaceBox(this.calculateFaceLocation(response))
+      }).catch(err => console.log(err)
     );
   }
   render() {
@@ -59,7 +82,7 @@ class App extends Component {
         <Logo/>
         <Rank/>
         <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
-        <FaceRecognition imageURL= {this.state.imageURL}/>
+        <FaceRecognition imageURL= {this.state.imageURL} box={this.state.box}/>
       </div>
     );
   }
