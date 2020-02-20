b7d163813ff46abb63b5bec20d74d145f7c4c0d5
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2020-02-20 18:09:36 +0100

    deploying to heroku

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ this.setState({imageUrl: this.state.input}, () => {
 */
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input});
-  fetch('http://localhost:3000/imageurl', {
+  fetch('https://afternoon-beach-63320.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -99,7 +99,7 @@ onButtonSubmit = () => {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://afternoon-beach-63320.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
